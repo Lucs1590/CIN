816be29a0533e5b2137d3a4dc9807e44aba0b40f
Lucas Brito <lucasbsilva29@gmail.com>
2020-04-22 22:16:01 -0300

    formula, temperatura e organização

--- search_methods.py
+++ search_methods.py
@@ -1,6 +1,6 @@
 import random
 from time import time
-# 1587493971.024905 para <
+# 1587603556.2999938 para <
 # 1587503056.092915 para >
 import matplotlib.pyplot as plt
 import math
@@ -19,7 +19,7 @@ class GenericClass(object):
         return float(point + abs(np.random.normal(0, 0.001, 1)))
 
     def func_g_x(self, x):
-        return (2**-2*(x-0.1/0.9)**2)*(math.sin(5*math.pi*x))**6
+        return 2 ** (-2 * ((((x-0.1) / 0.9)) ** 2)) * ((math.sin(5*math.pi*x)) ** 6)
 
     def plot_poits(self, data_cost, data_result):
         df = pd.DataFrame({"custo": data_cost, "resultado": data_result})
@@ -58,12 +58,6 @@ class HillClimbing(object):
         current_best_result = self.gc.get_random_point(seed)
         last_best_result = current_best_result
 
-        (current_best_result, cost) = self.evaluate_point(
-            current_best_result, last_best_result)
-
-        self.cost_list.append(cost)
-        self.results_list.append(current_best_result)
-
         while self.it < max_it:
             self.it_repeat += 1 if current_best_result == last_best_result else 0
             if (self.it_repeat == round(max_it/3)):
@@ -96,16 +90,11 @@ class SimulatedAnnealing(object):
         (self.it, self.it_repeat, self.cost, self.cost_list,
          self.results_list) = self.gc.set_default_values()
 
-    def run_simulated_annealing(self, max_it, temperature, seed):
+    def run_simulated_annealing(self, temperature, seed):
         current_best_result = self.gc.get_random_point(seed)
         last_best_result = current_best_result
 
-        while self.it < max_it:
-            # Fazer condição de parada com a temperatura minima tbm
-            self.it_repeat += 1 if current_best_result == last_best_result else 0
-            if (self.it_repeat == round(max_it/3)):
-                print("(SA) Motivo de parada: Sem melhorias!")
-                break
+        while temperature >= 0.1:
             last_best_result = current_best_result
             current_best_result = self.gc.disturb_point(current_best_result)
 
@@ -118,16 +107,16 @@ class SimulatedAnnealing(object):
             temperature = self.reduce_temperature(temperature, self.it)
             self.it += 1
 
-        print("(SA) Motivo de parada: Número máximo de interações atingido!") if max_it == self.it else ...
+        print("(SA) Motivo de parada: Temperatura foi zerada!")
         self.gc.plot_poits(self.cost_list, self.results_list)
-        return current_best_result, cost
+        return self.results_list[self.cost_list.index(max(self.cost_list))], max(self.cost_list)
 
     def evaluate_point(self, best_result, last_best_result, T):
         current_cost = self.gc.func_g_x(best_result)
         last_cost = self.gc.func_g_x(last_best_result)
         if last_cost < current_cost:
             return (best_result, current_cost)
-        elif random.random() < math.exp(last_cost - current_cost / T):
+        elif random.random() <= math.e**(last_cost - current_cost / T):
             return (best_result, current_cost)
         else:
             return (last_best_result, last_cost)
@@ -138,15 +127,18 @@ class SimulatedAnnealing(object):
 
 
 def main():
-    seed = 1587493971.024905
+    seed = time()
+
     max_it = 500
+    T = 500
     min_value = 0.9
+
     hc = HillClimbing()
     sa = SimulatedAnnealing()
 
     print("Semente: ", seed)
     (hc_best_result, hc_cost) = hc.run_hill_climbing(max_it, min_value, seed)
-    (sa_best_result, sa_cost) = sa.run_simulated_annealing(max_it, 100, seed)
+    (sa_best_result, sa_cost) = sa.run_simulated_annealing(T, seed)
     GenericClass.format_table(
         GenericClass, hc_best_result, hc_cost, sa_best_result, sa_cost)
 
