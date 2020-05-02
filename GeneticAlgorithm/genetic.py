d9fb621cf405f5ad4e03f0313095f17ed0cedd40
Lucas Brito <lucasbsilva29@gmail.com>
2020-05-02 20:38:54 -0300

    método de gerar população

--- GeneticAlgorithm/genetic.py
+++ GeneticAlgorithm/genetic.py
@@ -1,3 +1,7 @@
+from random import randint, seed
+from time import time
+
+
 class GeneticAlgorithm(object):
     def select(self, parameter_list):
         pass
@@ -17,17 +21,28 @@ class GeneticAlgorithm(object):
 
 class GenericClass(object):
     def to_bin(self, int_number):
-        return bin(int_number)
+        return format(int_number, '#014b')
 
     def to_number(self, bin_number):
         return int(bin_number, 2)
+
+    def generate_population(self, indiv_number, _seed=time()):
+        print(_seed)
+        seed(_seed)
+        population = []
+
+        for individual in range(indiv_number):
+            population.append(self.to_bin(randint(0, 3951)))
+
+        return population
 
 
 def main():
     genetic = GeneticAlgorithm()
     gc = GenericClass()
 
-    genetic.run_genetic_algorithm()
+    print(*gc.generate_population(8), sep='\n')
+    # genetic.run_genetic_algorithm()
 
 
 if __name__ == "__main__":
