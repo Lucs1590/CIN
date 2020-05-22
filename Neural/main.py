1edf7e58e9d5012ad3e6093ab70e35d220a9f6d8
Lucas Brito <lucasbsilva29@gmail.com>
2020-05-21 22:53:22 -0300

    base para o algoritmo

--- Neural/main.py
+++ Neural/main.py
@@ -1,10 +1,19 @@
-from wine.wine import Wine
-from iris.iris import Iris
+from aux import AuxiliaryClass
+from perceptron import Perceptron
 
 
 def main():
-    wine = Wine()
-    iris = Iris()
+    aux = AuxiliaryClass()
+
+    iris_data = aux.read_datas(
+        "/home/brito/Documentos/Mestrado/CIN/algoritmos/Neural/iris/iris.data")
+    print(iris_data)
+
+    wine_data = aux.read_datas(
+        "/home/brito/Documentos/Mestrado/CIN/algoritmos/Neural/iris/iris.data")
+    print(wine_data)
+
+    perceptron = Perceptron()
 
 
 if __name__ == "__main__":
