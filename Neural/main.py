fc9d487c19ad65f218a5a2dac420c1816c6d7ad9
Lucas Brito <lucasbsilva29@gmail.com>
2020-05-21 22:56:52 -0300

    dividindo entre wine e iris

--- Neural/main.py
+++ Neural/main.py
@@ -4,16 +4,21 @@ from perceptron import Perceptron
 
 def main():
     aux = AuxiliaryClass()
+    perceptron = Perceptron()
+    execute_iris(aux, perceptron)
+    execute_wine(aux, perceptron)
 
+
+def execute_iris(aux, perceptron):
     iris_data = aux.read_datas(
         "/home/brito/Documentos/Mestrado/CIN/algoritmos/Neural/iris/iris.data")
     print(iris_data)
 
+
+def execute_wine(aux, perceptron):
     wine_data = aux.read_datas(
         "/home/brito/Documentos/Mestrado/CIN/algoritmos/Neural/iris/iris.data")
     print(wine_data)
-
-    perceptron = Perceptron()
 
 
 if __name__ == "__main__":
