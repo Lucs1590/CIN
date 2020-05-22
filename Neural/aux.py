57a68b93b5ba28bb3692ec4637abac74bb230d6f
Lucas Brito <lucasbsilva29@gmail.com>
2020-05-22 00:23:06 -0300

    funções iniciais

--- Neural/aux.py
+++ Neural/aux.py
@@ -1,7 +1,27 @@
-import pandas as pd 
+import pandas as pd
 
 
 class AuxiliaryClass(object):
 
     def read_datas(self, path):
         return pd.read_csv(path)
+
+    def define_dataframe_column(self, dataframe, columns):
+        dataframe.columns = columns
+        return dataframe
+
+    def divide_dataset(self, dataset, train_percent, test_percent, validation_percent, seed):
+        dataset.sample(frac=1, random_state=int(seed))
+        train_dataset = dataset.iloc[
+            0:
+            round(len(dataset.index) * train_percent)
+        ]
+        test_dataset = dataset.iloc[
+            len(train_dataset.index):
+            round(len(dataset.index) * test_percent)
+        ]
+        validation_dataset = dataset.iloc[
+            len(test_dataset.index):
+            round(len(dataset.index) * validation_percent)
+        ]
+        return train_dataset, test_dataset, validation_dataset
