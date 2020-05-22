49dd54fa6afaae09ea9c320a3c295d2081149b39
Lucas Brito <lucasbsilva29@gmail.com>
2020-05-22 00:33:21 -0300

    ajustando divisão de datasets

--- Neural/aux.py
+++ Neural/aux.py
@@ -11,17 +11,17 @@ class AuxiliaryClass(object):
         return dataframe
 
     def divide_dataset(self, dataset, train_percent, test_percent, validation_percent, seed):
-        dataset.sample(frac=1, random_state=int(seed))
+        dataset = dataset.sample(frac=1, random_state=int(seed))
         train_dataset = dataset.iloc[
             0:
             round(len(dataset.index) * train_percent)
         ]
         test_dataset = dataset.iloc[
             len(train_dataset.index):
-            round(len(dataset.index) * test_percent)
+            len(train_dataset.index) + round(len(dataset.index) * test_percent)
         ]
         validation_dataset = dataset.iloc[
             len(test_dataset.index):
-            round(len(dataset.index) * validation_percent)
+            len(test_dataset.index) + round(len(dataset.index) * validation_percent)
         ]
         return train_dataset, test_dataset, validation_dataset
