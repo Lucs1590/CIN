0b2de449a1b8b2b61818e17a61854cc9fc6a8298
Lucas Brito <lucasbsilva29@gmail.com>
2020-05-25 12:16:35 -0300

    fazendo a definição dos pesos

--- Neural/perceptron.py
+++ Neural/perceptron.py
@@ -1,2 +1,19 @@
+import numpy as np
+
+
 class Perceptron(object):
-    pass
+
+    def run_perceptron(self, train_ds, test_ds, validation_ds):
+        weights = self.define_weights(train_ds)
+
+    def activation_func(self, sample, weights):
+        activation = weights[0]
+        for i in range(len(sample)-1):
+            activation += weights[i + 1] * sample[i]
+        return 1.0 if activation >= 0.0 else 0.0
+
+    def define_weights(self, dataset):
+        weights = []
+        for column in range(len(dataset.columns) - 1):
+            weights.append(np.random.normal(0, 0.001, 1))
+        return weights
