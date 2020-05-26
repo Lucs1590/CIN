49bbe1c1179a997c1801e5701e0443e5284ecbe7
Lucas Brito <lucasbsilva29@gmail.com>
2020-05-25 23:21:16 -0300

    definindo classses e adicionando bias

--- Neural/perceptron.py
+++ Neural/perceptron.py
@@ -4,16 +4,20 @@ import numpy as np
 class Perceptron(object):
 
     def run_perceptron(self, train_ds, test_ds, validation_ds):
+        train_ds = self.add_bias(train_ds)
         weights = self.define_weights(train_ds)
-
-    def activation_func(self, sample, weights):
-        activation = weights[0]
-        for i in range(len(sample)-1):
-            activation += weights[i + 1] * sample[i]
-        return 1.0 if activation >= 0.0 else 0.0
+        castes = self.define_castes(train_ds)
 
     def define_weights(self, dataset):
         weights = []
         for column in range(len(dataset.columns) - 1):
             weights.append(np.random.normal(0, 0.001, 1))
         return weights
+
+    def add_bias(self, dataset):
+        dataset['bias'] = 0
+        return dataset
+
+    def define_castes(self, dataset, column_name="class"):
+        clusters = dataset[column_name].unique()
+        return list(clusters)
