55459b7a4fd31c5a9d68d2e0ffdf6951e26e8043
Lucas Brito <lucasbsilva29@gmail.com>
2020-05-25 23:32:12 -0300

    organizando o loop

--- Neural/main.py
+++ Neural/main.py
@@ -4,24 +4,28 @@ from time import time
 
 
 def main():
-    seed = time()
-    print("Seed: ", seed)
     aux = AuxiliaryClass()
     perceptron = Perceptron()
-    execute_iris(aux, perceptron, seed)
+
+    seed = time()
+    max_it = 5000
+    print("Seed: ", seed)
+
+    execute_iris(aux, perceptron, seed, max_it)
     # execute_wine(aux, perceptron, seed)
 
 
-def execute_iris(aux, perceptron, seed):
+def execute_iris(aux, perceptron, seed, max_it):
     iris_data = aux.read_datas("Neural/iris/iris.data")
     iris_data = aux.define_dataframe_column(
         iris_data, ["sepal_length", "sepal_width", "petal_length", "petal_width", "class"])
     (train_dataset, test_dataset, validation_dataset) = aux.divide_dataset(
         iris_data, 0.7, 0.15, 0.15, seed)
-    perceptron.run_perceptron(train_dataset, test_dataset, validation_dataset)
+    perceptron.run_perceptron(
+        train_dataset, test_dataset, validation_dataset, max_it)
 
 
-def execute_wine(aux, perceptron, seed):
+def execute_wine(aux, perceptron, seed, max_it):
     wine_data = aux.read_datas("Neural/wine/wine.data")
     print(wine_data)
 
--- Neural/perceptron.py
+++ Neural/perceptron.py
@@ -3,10 +3,16 @@ import numpy as np
 
 class Perceptron(object):
 
-    def run_perceptron(self, train_ds, test_ds, validation_ds):
+    def run_perceptron(self, train_ds, test_ds, validation_ds, max_it):
         train_ds = self.add_bias(train_ds)
         weights = self.define_weights(train_ds)
         castes = self.define_castes(train_ds)
+
+        it = 0
+        error = 0
+
+        while it <= max_it and error > 0:
+            it += 1
 
     def define_weights(self, dataset):
         weights = []
