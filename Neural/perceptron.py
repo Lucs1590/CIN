014a366c3774b0b60521300b2ec1a62ef049d5e2
Lucas Brito <lucasbsilva29@gmail.com>
2020-05-26 23:02:19 -0300

    organizando o código

--- Neural/perceptron.py
+++ Neural/perceptron.py
@@ -5,20 +5,15 @@ class Perceptron(object):
 
     def run_perceptron(self, train_ds, test_ds, validation_ds, max_it):
         train_ds = self.add_bias(train_ds)
-        weights = self.define_weights(train_ds)
         castes = self.define_castes(train_ds)
+        weights = self.define_weights(train_ds, castes)
 
         it = 0
+
         error = 0
 
-        while it <= max_it and error > 0:
+        while it <= max_it and error >= 0:
             it += 1
-
-    def define_weights(self, dataset):
-        weights = []
-        for column in range(len(dataset.columns) - 1):
-            weights.append(np.random.normal(0, 0.001, 1))
-        return weights
 
     def add_bias(self, dataset):
         dataset['bias'] = 0
@@ -27,3 +22,16 @@ class Perceptron(object):
     def define_castes(self, dataset, column_name="class"):
         clusters = dataset[column_name].unique()
         return list(clusters)
+
+    def define_weights(self, dataset, castes):
+        weights = []
+        for column in range(len(dataset.columns) - 1):
+            i = 0
+            list_of_weights = []
+
+            while i < len(castes):
+                list_of_weights.append(float(np.random.normal(0, 0.001, 1)))
+                i += 1
+            weights.append(list_of_weights)
+
+        return weights
