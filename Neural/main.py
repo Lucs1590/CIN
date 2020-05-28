be3a3f12713197e527c9e121ccab4a9607e9d674
Lucas Brito <lucasbsilva29@gmail.com>
2020-05-27 22:45:47 -0300

    colocando as informações de vinho

--- Neural/main.py
+++ Neural/main.py
@@ -27,7 +27,12 @@ def execute_iris(aux, perceptron, seed, max_it):
 
 def execute_wine(aux, perceptron, seed, max_it):
     wine_data = aux.read_datas("Neural/wine/wine.data")
-    print(wine_data)
+    wine_data = aux.define_dataframe_column(
+        wine_data, ["alcohol", "malic_acid", "ash", "alcalinity_of_ash", "magnesium", "total_phenols", "flavanoids", "nonflavanoid_phenols", "proanthocyanins", "color_intensity", "hue", "od280_od315_of_diluted_wines", "proline", "class"])
+    (train_dataset, test_dataset, validation_dataset) = aux.divide_dataset(
+        wine_data, 0.7, 0.15, 0.15, seed)
+    perceptron.run_perceptron(
+        train_dataset, test_dataset, validation_dataset, max_it)
 
 
 if __name__ == "__main__":
