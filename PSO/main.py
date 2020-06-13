51b1810ad01e3f4647c177d74aab3262d628c2e2
Lucas Brito <lucasbsilva29@gmail.com>
2020-06-13 17:50:25 -0300

    inicio do trabalho de pso

---
+++ PSO/main.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class AuxiliaryClass(object):
+    def __init__(self):
+        ...
+
+
+class PSOClass(object):
+    def __init__(self):
+        ...
+
+    def generate_population(self, numb_particles):
+        ...
+
+
+def main():
+    ...
+
+
+if __name__ == "__main__":
+    main()
