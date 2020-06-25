4cdde02ab314625d77022a1f7f3995a90e9465ce
Lucas Brito <lucasbsilva29@gmail.com>
2020-06-25 09:24:00 -0300

    gerando populações, velocidades e main

--- PCO/main.py
+++ PCO/main.py
@@ -1,23 +1,41 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from random import uniform, seed
+from time import time
 
 
 class AuxiliaryClass(object):
     def __init__(self):
         ...
 
-    def generate_population(self, numb_particles):
-        ...
+    def generate_population(self, indiv_number, dimensions):
+        populations = []
+        i = 0
+        while i < dimensions:
+            population = []
+
+            for individual in range(indiv_number):
+                population.append(uniform(-5, 5))
+
+            populations.append(population)
+            i += 1
+
+        return populations
+
+    def define_seed(self, _seed=time()):
+        print("Seed: ", _seed)
+        seed(_seed)
 
 
 class PSOClass(object):
     def __init__(self):
         self.aux = AuxiliaryClass()
 
-    def runPSO(self, particles, max_it, AC1, AC2, v_min, v_max):
+    def runPSO(self, particles, max_it, AC1, AC2, v_min, v_max, dimensions):
         x = 0
         it = 0
-        speed = self.generate_speed(v_min, v_max)
+        particles = self.aux.generate_population(particles, dimensions)
+        speeds = self.generate_speed(v_min, v_max)
 
         while it < max_it:
 
@@ -40,12 +58,12 @@ class PSOClass(object):
                         best_neighbor = optimal_of_neighbor
                 i += 1
 
-                speed = self.update_speed()
+                speeds = self.update_speeds()
 
             it += 1
 
     def generate_speed(self, v_min, v_max):
-        ...
+        return uniform(v_min, v_max)
 
     def get_neighbors(self, curr_particle, flock):
         ...
@@ -53,7 +71,7 @@ class PSOClass(object):
     def get_best_neightbor(self, neighbors):
         ...
 
-    def update_speed(self):
+    def update_speeds(self):
         ...
 
 
@@ -66,7 +84,15 @@ class ACOClass(object):
 
 
 def main():
-    ...
+    aux = AuxiliaryClass()
+    pso = PSOClass()
+    (n_indiv, max_it, AC1, AC2, v_min, v_max, dimensions) = (
+        8, 500, 2.05, 2.05, 64, 96, 2)
+    aux.define_seed()
+
+    start_time = time()
+    pso.runPSO(n_indiv, max_it, AC1, AC2, v_min, v_max, dimensions)
+    print("Tempo: ", time() - start_time)
 
 
 if __name__ == "__main__":
