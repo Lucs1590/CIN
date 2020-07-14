8d1122d7145c0f8d11a15866ff33e1019de21aaf
Lucas Brito <lucasbsilva29@gmail.com>
2020-07-13 21:46:28 -0300

    estrutura inicial

--- ACO/main.py
+++ ACO/main.py
@@ -1,14 +1,45 @@
+from time import time
+from random import uniform, seed, random
+
+import tsplib95
+import pandas as pd
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# from functools import reduce
+# import operator
+
+
 class Auxiliary(object):
-    ...
+    def read_file(self, path):
+        _file = tsplib95.load(path)
+        return pd.DataFrame.from_dict(_file.node_coords, orient='index')
+
+    def define_seed(self, _seed=time()):
+        print("Seed: ", _seed)
+        seed(_seed)
 
 
 class ACO(object):
-    def __init__(self, max_it, num_ants, pheromone):
+    def __init__(self):
+        self.aux = Auxiliary()
+
+    def run_ACO(self, max_it, num_ants, pheromone):
         ...
 
 
-def main(self):
-    ...
+def main():
+    aux = Auxiliary()
+    aco = ACO()
+
+    aux.define_seed()
+    start_time = time()
+    aa = aux.read_file("ACO/berlin52.tsp")
+
+    end_time = 0
+
+    exec_time = end_time - start_time
+    print("Tempo: ", exec_time)
 
 
 if __name__ == "__main__":
