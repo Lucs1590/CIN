13f10608660eba642d0ef56b5cb7faa56394a29e
Lucas Brito <lucasbsilva29@gmail.com>
2020-07-13 23:34:18 -0300

    criando base do algoritmo

--- ACO/main.py
+++ ACO/main.py
@@ -26,12 +26,38 @@ class Auxiliary(object):
         plt.title("Initial Graph")
         plt.show()
 
+
 class ACO(object):
     def __init__(self):
         self.aux = Auxiliary()
 
     def run_ACO(self, max_it, num_ants, pheromone):
-        ...
+        it = 0
+        min_ways = []
+
+        tingling = self.aux.define_tingling(num_ants)
+        best_solution = self.define_best_solution(tingling)
+
+        while it < max_it:
+            idx = 0
+
+            while idx < len(tingling):
+                # prev_best_solution = curr_best_solution
+                curr_best_solution = self.compare_solutions(
+                    curr_best_solution, prev_best_solution)
+                idx += 1
+
+            best_solution = self.compare_solutions(
+                best_solution, curr_best_solution)
+            self.upgrade_ways()
+
+            it += 1
+
+        end_time = time()
+        best_sequence = min(min_ways)
+        best_it = min_ways.index(best_sequence)
+
+        return end_time, best_sequence, best_it
 
 
 def main():
@@ -44,10 +70,14 @@ def main():
     aux.define_seed()
     start_time = time()
 
-    end_time = 0
+    (end_time, best_sequence, it) = aco.run_ACO(500, 50, 0.4)
 
     exec_time = end_time - start_time
+
+    aux.plot_cities(best_sequence)
     print("Tempo: ", exec_time)
+    print("Melhor caminho: ", best_sequence)
+    print("Nº da Interação:", it)
 
 
 if __name__ == "__main__":
