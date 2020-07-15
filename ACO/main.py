394082a5011cdcdd410064a323fb63e27c518a29
Lucas Brito <lucasbsilva29@gmail.com>
2020-07-15 00:02:25 -0300

    utilizando o arquivo original e definindo formigueiro

--- ACO/main.py
+++ ACO/main.py
@@ -13,8 +13,11 @@ import matplotlib.pyplot as plt
 
 class Auxiliary(object):
     def read_file(self, path):
-        _file = tsplib95.load(path)
+        _file = self.read_original_file(path)
         return pd.DataFrame.from_dict(_file.node_coords, orient='index')
+
+    def read_original_file(self, path):
+        return tsplib95.load(path)
 
     def define_seed(self, _seed=time()):
         print("Seed: ", _seed)
@@ -26,25 +29,36 @@ class Auxiliary(object):
         plt.title("Initial Graph")
         plt.show()
 
+    def define_tingling(self, num_ants, cities):
+        idx = 0
+        tingling = []
+        while idx < num_ants:
+            chosen = cities.sample(1)
+            tingling.append(chosen.values)
+            cities = cities.drop(chosen.index)
+            idx += 1
+        return tingling
+
 
 class ACO(object):
     def __init__(self):
         self.aux = Auxiliary()
 
-    def run_ACO(self, max_it, num_ants, pheromone):
+    def run_ACO(self, max_it, num_ants, pheromone, cities):
         it = 0
+        best_solution = 0
         min_ways = []
-
-        tingling = self.aux.define_tingling(num_ants)
-        best_solution = self.define_best_solution(tingling)
 
         while it < max_it:
             idx = 0
 
-            while idx < len(tingling):
+            tingling = self.aux.define_tingling(num_ants, cities)
+            
+            while idx < len(cities)-1:
                 # prev_best_solution = curr_best_solution
                 curr_best_solution = self.compare_solutions(
                     curr_best_solution, prev_best_solution)
+
                 idx += 1
 
             best_solution = self.compare_solutions(
@@ -65,12 +79,12 @@ def main():
     aco = ACO()
 
     cities = aux.read_file("ACO/berlin52.tsp")
-    aux.plot_cities(cities)
+    # aux.plot_cities(cities)
 
     aux.define_seed()
     start_time = time()
 
-    (end_time, best_sequence, it) = aco.run_ACO(500, 50, 0.4)
+    (end_time, best_sequence, it) = aco.run_ACO(500, len(cities), 0.4, cities)
 
     exec_time = end_time - start_time
 
