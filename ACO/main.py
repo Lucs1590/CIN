8de4e05f438b528eae4b12a7fff9f6622e87ae35
Lucas Brito <lucasbsilva29@gmail.com>
2020-07-15 22:14:01 -0300

    movimento

--- ACO/main.py
+++ ACO/main.py
@@ -39,6 +39,11 @@ class Auxiliary(object):
             idx += 1
         return tingling
 
+    def get_euclidean_distance(self, ant, next_move_coord):
+        curr_ant_coord = (ant[0], ant[1])
+        next_coord = (next_move_coord[0], next_move_coord[1])
+        return tsplib95.distances.euclidean(curr_ant_coord, next_coord)
+
 
 class ACO(object):
     def __init__(self):
@@ -46,23 +51,22 @@ class ACO(object):
 
     def run_ACO(self, max_it, num_ants, pheromone, cities):
         it = 0
-        best_solution = 0
+        best_solution = []
         min_ways = []
+
+        self.problem = self.aux.read_original_file("ACO/berlin52.tsp")
 
         while it < max_it:
             idx = 0
 
+            ants_distance = [0] * num_ants
             tingling = self.aux.define_tingling(num_ants, cities)
-            
-            while idx < len(cities)-1:
-                # prev_best_solution = curr_best_solution
-                curr_best_solution = self.compare_solutions(
-                    curr_best_solution, prev_best_solution)
 
+            while idx < len(cities)-1:
+                best_solution = self.move_ants(tingling)
                 idx += 1
 
-            best_solution = self.compare_solutions(
-                best_solution, curr_best_solution)
+            best_solution = self.compare_solutions()
             self.upgrade_ways()
 
             it += 1
@@ -72,6 +76,34 @@ class ACO(object):
         best_it = min_ways.index(best_sequence)
 
         return end_time, best_sequence, best_it
+
+    def move_ants(self, ants):
+        idx = 0
+
+        while idx < len(ants):
+            curr_ant = ants[idx]
+            probability = self.get_probability_move(curr_ant)
+
+            idx += 1
+
+    def get_probability_move(self, ant):
+        idx = 0
+        curr_ant_probabilities = []
+        ant = ant.tolist()[0]
+
+        while idx < len(list(self.problem.get_nodes())):
+            next_move_idx = list(self.problem.get_edges())[idx][1]
+            next_move_coord = self.problem.node_coords[next_move_idx]
+            if ant != next_move_coord:
+                distance = self.aux.get_euclidean_distance(
+                    ant, next_move_coord)
+
+                visibility = 1/distance
+                curr_ant_probabilities = ...
+
+            idx += 1
+
+        return curr_ant_probabilities
 
 
 def main():
